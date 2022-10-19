e403756704f83af7bd47f553582a035793d07f3c
Roger Laura <roylauracaceres@gmail.com>
2022-10-18 23:39:45 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleColaCircular/Cola.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleColaCircular
+{
+    internal class Cola
+    {
+        private int ultimo,primero,maxcola;
+        private string[] items;
+        public Cola(int maxcola)
+        {
+            this.ultimo = -1;
+            this.primero = -1;
+            this.maxcola= maxcola;
+            this.items = new string[this.maxcola];
+        }
+        public bool colavacia()
+        {
+            if(this.ultimo == -1 && this.primero == -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public  bool colallena()
+        {
+            if((this.ultimo==this.maxcola-1 && this.primero == 0) || this.ultimo+1 == this.primero)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+                                        //60
+        public void InsertarItem(string dato)
+        {
+            if (this.colallena())
+            {
+                Console.WriteLine("LA COLA ESTA LLENA");
+            }
+            else
+            {
+                
+                if (this.ultimo == this.maxcola - 1)
+                {
+                    this.ultimo = 0;
+
+
+                }
+                else
+                {
+                    
+                    this.ultimo++;
+                }
+               
+                this.items[this.ultimo] = dato;
+                if (this.primero == -1)
+                {
+                    this.primero = 0;
+                }
+            }
+        }
+
+        public void EliminarItem()
+        {
+            if (this.colavacia())
+            {
+                Console.WriteLine("\tLA COLA ESTA VACIA");
+            }
+            else
+            {
+                Console.WriteLine("\tSE ELIMINO EL ELEMENTO " + this.items[this.primero]);
+                this.items[this.primero] = "";
+                if (this.primero == this.ultimo)
+                {
+                    this.primero = -1;
+                    this.ultimo = -1;
+
+                }
+                else
+                {
+                    if (this.primero == this.maxcola - 1)
+                    {
+                        this.primero = 0;
+                    }
+                    else
+                    {
+                        //1+1=2
+                        this.primero++;
+                    }
+                }
+            }
+        }
+        public void ListarItems()
+        {
+            if (this.colavacia())
+            {
+                Console.WriteLine("\tLA COLA ESTA VACIA");
+            }
+            else
+            {
+                
+                for (int i = 0; i < this.maxcola; i++)
+                {
+                    if (this.items[i]!=null)
+                    {
+                        Console.WriteLine("\t[" + i + "] => " + this.items[i]);
+                    }
+                    
+                    
+                    
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleColaCircular/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleColaCircular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int  opcion;
+            string dato;
+            Cola cola = new Cola(4);
+            do {
+                Console.WriteLine("\t===================== [ MENU COLA CIRCULAR ] =====================");
+                Console.WriteLine("\t\t1.- INSERTAR");
+                Console.WriteLine("\t\t2.- ELIMINAR");
+                Console.WriteLine("\t\t3.- LISTAR");
+                Console.WriteLine("\t\t4.- SALIR");
+                Console.Write("\tELIGIR UNA OPCION: ");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("\tDIGITE UN VALOR: ");
+                        dato=Console.ReadLine();
+                        cola.InsertarItem(dato);
+                        break;
+                    case 2:
+                        cola.EliminarItem();
+                        break;
+                    case 3:
+                        cola.ListarItems();
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        break;
+
+                }
+                Console.ReadKey(true);
+                Console.Clear();
+            } while (opcion != 4);
+        }
+    }
+}
